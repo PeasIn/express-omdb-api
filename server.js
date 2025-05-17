8b6eeb753fb3cb767e3e8b95627ffc32e043925f
PeasIn <74363821+PeasIn@users.noreply.github.com>
2025-05-17 12:16:47 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 5000;
+const OMDB_API_KEY = process.env.OMDB_API_KEY
+
+app.get("/", (req, res)=>{
+    res.send("idk wtf im doing")
+})
+
+app.get("/search", async (req, res)=>{
+    const movie = req.query.movie;
+    if (!movie)
+        return res.status(400).json({ error : "idiotic ass forgot the movie query"});
+
+    try{
+        const response = await axios.get(`http://www.omdbapi.com/?s=${movie}&apikey=${OMDB_API_KEY}`);
+        res.status(200).json(response.data);
+    } catch (err)
+    {
+        res.status(404).json({ error : "stupid ass forgot that this movie does NOT exist"})
+    }
+})
+
+// curl -X "GET" "http://localhost:5000/details?movieId=tt1517268"
+app.get("/details", async (req, res)=>{
+    const movieId = req.query.movieId;
+    if (!movieId)
+        return res.status(400).json({ error : "idiotic ass forbot about the movie ID query"});
+
+    // diisi dan dilanjutkan untuk request detail movie
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is PROBABLY running(cant be so sure) on http://localhost:${PORT}`)
+});
